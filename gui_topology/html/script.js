3cad22c8723d472ebcee94210743d47f836f60ea
Giorgia Gabardi <giorgia.gabardi@studenti.unitn.it>
2025-02-17 17:20:52 +0100

    modifica nome

--- gui_topology/html/script.js
+++
@@ -1,9 +0,0 @@
-// Ottieni il bottone
-const button = document.getElementById('nightModeToggle');
-
-// Aggiungi un evento al bottone
-button.addEventListener('click', function() {
-    // Toggle della classe 'night-mode' sul body
-    document.body.classList.toggle('night-mode');
-
-});

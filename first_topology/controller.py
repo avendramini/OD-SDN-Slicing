36db397fc1fdc93bc949ab3c4a5abfe124b7fb6f
Alberto <alberto.vendra@gmail.com>
2025-02-20 17:47:51 +0100

    fix

--- first_topology/controller.py
+++ first_topology/controller.py
@@ -187,7 +187,7 @@ class ControllerServer(ControllerBase):
         try:
             mode_data = req.json if req.body else {}
             mode = int(mode_data.get('mode')) if mode_data.get('mode') else None
-            if mode not in [self.DAY, self.NIGHT]:
+            if mode not in [self.DAY, self.NIGHT] or mode == self.active_mode:
                 return Response(status=400, body="Invalid mode")
             self.active_mode = mode
             active_slices = [i+1 for i, active in enumerate(self.mappers[self.active_mode].active_slice) if active]
